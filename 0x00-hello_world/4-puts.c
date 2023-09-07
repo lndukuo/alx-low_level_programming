9a7c4cc8993a76d27d99880a5077296982108f98
lndukuo <leonardndukuo@gmail.com>
2023-09-06 23:37:01 -0700

    My first C programm code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * this is a c program printing using the puts function
+ */
+int main(void)
+{
+	puts("\"Programming is like biulding a multilingual puzzle");
+	return (0)
+}
