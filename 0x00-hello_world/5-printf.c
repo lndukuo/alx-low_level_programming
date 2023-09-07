0e3b69458be20178bc88bbe24c135d4f8e5fc673
lndukuo <leonardndukuo@gmail.com>
2023-09-07 00:31:31 -0700

    second function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - this is the start point of the programm
+ *
+ * Description - A C program that prints using the printf function
+ *
+ * Return: Always  0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
