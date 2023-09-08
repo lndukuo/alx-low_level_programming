a8a446499db0e029d80d9f66cbb383a155dff8d3
lndukuo <leonardndukuo@gmail.com>
2023-09-08 14:49:09 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower case followed by a new line.
+ *
+ * Return: Always 0 (Success)**
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
