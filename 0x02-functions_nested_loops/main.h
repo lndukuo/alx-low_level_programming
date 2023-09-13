d237a8772e72a612170ece9845b25cd038ef82f1
lndukuo <leonardndukuo@gmail.com>
2023-09-13 13:46:32 -0700

    wtf have i done????

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,7 @@
-/**
- *
- * This is a file containing header files and functions
- * declaration
- *
- * */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -11,10 +9,12 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int x);
-int print_last_digit(int y);
+int _abs(int);
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif /* MAIN_H */
