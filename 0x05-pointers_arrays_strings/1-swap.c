319a9d8aebcb6f37eabce221b15df3e1ae3ebd6c
lndukuo <leonardndukuo@gmail.com>
2023-09-18 02:47:54 -0700

    dear lord let it work

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,5 @@
-#include <main.h>
 /**
- * swap_int - swaps the values of two integers
+ * swap_int - it swaps the values of two integers
  *		using two input parameters
  *
  * @a: input parameter 1
