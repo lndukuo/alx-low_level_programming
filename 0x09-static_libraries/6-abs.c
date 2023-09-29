90f441860e1e15967ec8034bb66ecedd04b5f7fe
lndukuo <leonardndukuo@gmail.com>
2023-09-29 15:11:34 +0300

    painful task

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ * @x: integer
+ * Return: the absolute val of integer
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (-x);
+}
