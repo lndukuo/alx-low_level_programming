31cb9d6fdcca34832c12f36c7dc9c21ab3ead811
lndukuo <leonardndukuo@gmail.com>
2023-10-19 14:05:34 +0300

    trial 2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,15 @@
-section .data
-    helloMsg db "Hello, Holberton", 0
-    format db "%s", 0
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-    extern printf
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-    global main
-    main:
-        push rbp
-        mov rdi, format
-        mov rsi, helloMsg
-        call printf
-        add rsp, 8  ; clean up the stack
-        xor eax, eax  ; return 0
-        pop rbp
-        ret
+	mov eax, 0
+	ret
