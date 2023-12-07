660acc5d429061f516de6e8de96260f295bde560
lndukuo <leonardndukuo@gmail.com>
2023-12-07 18:07:49 +0300

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - works the number of elements in a doubly linked lists.
+ * @h: pointer to the head of the list.
+ * Return: Number of nodes in the list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return count;
+}
